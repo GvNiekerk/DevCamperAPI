73dfac79dce7d92def0044d7f01e323da71232d7
GvNiekerk <gvniekerk011@gmail.com>
2021-09-21 19:20:45 +0200

    Added security stuff

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -39,3 +39,73 @@ exports.getReview = asyncHandler(async (req, res, next) => {
         data: review
     });
 });
+
+// @desc        Add review
+// @route       POST /api/v1/bootcamps/:bootcampId/reviews
+// @access      Private
+exports.addReview = asyncHandler(async (req, res, next) => {
+    req.body.bootcamp = req.params.bootcampId;
+    req.body.user = req.user.id;
+
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+    if (!bootcamp) {
+        return next(new ErrorResponse(`Bootcamp not found for id ${req.params.bootcampId}`, 404));
+    }
+
+    const review = await Review.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        data: review
+    });
+});
+
+// @desc        Update review
+// @route       PUT /api/v1/bootcamps/reviews/:id
+// @access      Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+    let review = await Review.findById(req.params.id);
+
+    if (!review) {
+        return next(new ErrorResponse(`Review not found for id ${req.params.id}`, 404));
+    }
+
+    //Ensure current user is user who created this review
+    if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`Not authorized to update review`, 401));
+    }
+
+    review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        success: true,
+        data: review
+    });
+});
+
+// @desc        Delete review
+// @route       Delete /api/v1/bootcamps/reviews/:id
+// @access      Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+    const review = await Review.findById(req.params.id);
+
+    if (!review) {
+        return next(new ErrorResponse(`Review not found for id ${req.params.id}`, 404));
+    }
+
+    //Ensure current user is user who created this review
+    if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`Not authorized to delete review`, 401));
+    }
+
+    await review.remove();
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
+});
--- middleware/auth.js
+++ middleware/auth.js
@@ -8,12 +8,12 @@ exports.protect = asyncHandler(async (req, res, next) => {
     let token;
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        //Set token from bearer token
         token = req.headers.authorization.split(' ')[1];
+        //Set token from cookie
+    } else if (req.cookies.token) {
+        token = req.cookies.token;
     }
-
-    // else if (req.cookies.token) {
-    //     token = req.cookies.token;
-    // }
 
     //Make sure token exists
     if (!token) {
